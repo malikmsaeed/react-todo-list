e151f73bab072298693ac7456f8ea5ddfe9cee8e
malikmsaeed <malikmsaeed88@gmail.com>
2019-08-18 12:30:53 +0500

    changing function to class in app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <TodoInput />
-        <TodoList />
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <TodoInput />
+          <TodoList />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
