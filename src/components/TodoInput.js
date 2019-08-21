cb1e2b5a2597bf8f0c8d7b7e3e4ba8978d0b3612
malikmsaeed <malikmsaeed88@gmail.com>
2019-08-21 11:47:40 +0500

    Add TodoInput Structure

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,31 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
-      <div>
-        <h2>Hello From Todo Input</h2>
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              placeholder="add todo item"
+              className="form-control text-capitalize"
+              onChange={handleChange}
+              value={item}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary text-uppercase mt-3 btn-block"
+          >
+            Add Item
+          </button>
+        </form>
       </div>
     );
   }
